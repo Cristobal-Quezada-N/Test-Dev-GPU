0c10c808cbfef9434fd4df72a7ca62397151b366
Josue Quileñan <josue.quilenan@usach.cl>
2025-08-25 22:46:46 -0400

    feat(backend): estructura de parte backend
    Estructurar los contenidos en el directorio `backend`.

---
+++ backend/src/main/java/usach/hackaton/gpu/GpuApplication.java
@@ -0,0 +1,13 @@
+package usach.hackaton.gpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpuApplication.class, args);
+	}
+
+}
