9c5ba0958b9032ec6be97c00e911360ea9a9921e
Cristóbal <development.cristobal@gmail.com>
2025-08-25 23:44:05 -0400

    feat(db): crear esquema básico para base de datos
    Entidades:
    
    - Auth_factor
    - App_user
    - Role
    - User_status
    - Loan_status
    - Loan
    - Item

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,52 @@
+CREATE TYPE auth_factor_type as ENUM (
+    'totp'
+);
+
+CREATE TABLE User_status (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(50),
+    name VARCHAR(20)
+);
+
+CREATE TABLE Role (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(50),
+    name VARCHAR(20)
+);
+
+CREATE TABLE Item (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE App_user (
+    id VARCHAR(50) PRIMARY KEY,
+    role_id INTEGER NOT NULL REFERENCES Role(id),
+    status_id INTEGER NOT NULL REFERENCES User_status(id),
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Auth_factor (
+    id SERIAL PRIMARY KEY,
+    user_id VARCHAR(50) NOT NULL REFERENCES App_user(id),
+    type auth_factor_type NOT NULL,
+    used BOOLEAN NOT NULL,
+    creation_date DATE NOT NULL,
+    expiration_date DATE NOT NULL
+);
+
+CREATE TABLE Loan (
+    id SERIAL PRIMARY KEY,
+    user_id VARCHAR(50) NOT NULL REFERENCES App_user(id),
+    item_id INTEGER NOT NULL,
+    status_id INTEGER NOT NULL,
+    date DATE NOT NULL,
+    deadline DATE NOT NULL
+);
+
+CREATE TABLE Loan_Status (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(50),
+    name VARCHAR(20)
+);
