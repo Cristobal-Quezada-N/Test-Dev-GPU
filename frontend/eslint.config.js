78a2c35bc630530170b764e0f3e2d298f1133d2e
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-08-27 16:44:20 -0400

    chore(eslint): implementar formatos y estilos con eslint
    Implementado con husky y eslint.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,44 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 import vuetify from 'eslint-config-vuetify'
+import unusedImports from 'eslint-plugin-unused-imports'
+import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
 
-export default vuetify()
+export default vuetify(
+  {},
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'import/no-duplicates': 'off',
+      'no-duplicate-imports': ['error', { allowSeparateTypeImports: true }],
+
+      '@typescript-eslint/consistent-type-imports': ['warn', { fixStyle: 'inline-type-imports' }],
+
+      'unused-imports/no-unused-imports': 'warn',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+
+      'vue/multi-word-component-names': 'warn',
+      'vue/attributes-order': 'warn',
+      'unicorn/no-empty-file': 'warn',
+    },
+  },
+)
