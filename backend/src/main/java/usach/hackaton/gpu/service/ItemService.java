5e175b1009f69418b43e19a15488b1bf8ed074c3
Gerardo Perez <gerardo.perez@usach.cl>
2025-08-26 23:55:01 -0400

    feat(db): Inventario manejable, puedes eliminar, editar y agregar juegos

---
+++ backend/src/main/java/usach/hackaton/gpu/service/ItemService.java
@@ -0,0 +1,41 @@
+package usach.hackaton.gpu.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import usach.hackaton.gpu.entities.Item;
+import usach.hackaton.gpu.repositories.ItemRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public Item saveItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Optional<Item> getItemById(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    public void deleteItem(Long id) {
+        itemRepository.deleteById(id);
+    }
+
+    public Optional<Item> updateItem(Long id, Item updatedItem) {
+        return itemRepository.findById(id).map(item -> {
+            item.setName(updatedItem.getName());
+            item.setStock(updatedItem.getStock());
+            item.setAvailable(updatedItem.getAvailable());
+            return itemRepository.save(item);
+        });
+    }
+}
