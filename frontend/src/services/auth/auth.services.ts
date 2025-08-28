f4d97cd8ca0a1c905e27ed8cbca8720d576a25b7
Josue Quileñan <josue.quilenan@usach.cl>
2025-08-28 12:05:06 -0400

    feat(front): First attempt of HU1. Without any testing tho.

--- frontend/src/services/auth/auth.services.ts
+++ frontend/src/services/auth/auth.services.ts
@@ -2,7 +2,15 @@ import { authApi } from './auth.api'
 
 export const authService = {
   login: async (email: string, password: string) => {
-    const data = await authApi.mockLogin(email, password)
-    return data
+  // Para #MOCK
+  //   const data = await authApi.mockLogin(email, password)
+  //   return data
+  // },
+    const response = await authApi.post('/api/auth/login', { email, password })
+    return response.data // { token: "..."}
+  },
+  register: async (email: string, password: string) => {
+    const response = await authApi.post('/api/auth/register', { email, password })
+    return response.data
   },
 }
