8362d9cb352f8960277affc7e820d20d40fe40de
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-08-27 22:44:21 -0400

    refactor(ItemsTable): extraer componente de *Items*
    Extraer componente de *Items* para views:
    - *Prestamo*
    - *Inventario*
    - *Solicitar*
    - *Solicitudes*
    - *Usuarios*
    
    commit 26f63aa
    Author: Gerardo Pérez <gerardo.perez@usach.cl>
    Date:   Wed Aug 27 16:06:30 2025 -0400
    
        feat(frontend): Prestamo user view
    
    commit e7a84de
    Author: Gerardo Pérez <gerardo.perez@usach.cl>
    Date:   Tue Aug 26 23:57:16 2025 -0400
    
        feat(frontend): Inventario view

---
+++ frontend/src/components/ItemsTable.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-container :class="containerClass" :fluid="fluid">
+    <v-card class="pa-4" elevation="2">
+      <!-- Titulo -->
+      <v-card-title>
+        <slot name="title">
+          {{ title }}
+        </slot>
+        <v-spacer />
+        <v-text-field
+          v-model="searchModel"
+          clearable
+          hide-details
+          label="Buscar"
+          prepend-icon="mdi-magnify"
+        />
+      </v-card-title>
+
+      <!-- Elementos en Top -->
+      <slot name="top" />
+
+      <!-- Tabla -->
+      <v-data-table-virtual
+        class="elevation-1"
+        fixed-header
+        :headers="headers"
+        :items="items"
+        :search="searchModel"
+        v-bind="$attrs"
+      >
+        <!-- Stock -->
+        <template #item.stock="{ item }">
+          <slot :item="item" name="item.stock">
+            <span>{{ item.stock }}</span>
+          </slot>
+        </template>
+
+        <!-- Available -->
+        <template #item.available="{ item }">
+          <slot :item="item" name="item.available">
+            <v-chip :color="item.available ? availableTrueColor : availableFalseColor">
+              {{ item.available ? availableTrueText : availableFalseText }}
+            </v-chip>
+          </slot>
+        </template>
+
+        <!-- Acciones -->
+        <template #item.actions="{ item }">
+          <slot :item="item" name="item.actions" />
+        </template>
+      </v-data-table-virtual>
+
+      <!-- Elementos en Bottom -->
+      <slot name="bottom" />
+    </v-card>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  const searchModel = defineModel<string>('search', { default: '' })
+  type Header = {
+    title: string
+    key: string
+    align?: 'start' | 'end' | 'center'
+    sortable?: boolean
+    width?: string | number
+  }
+
+  const props = withDefaults(defineProps<{
+    // Contenedor
+    fluid?: boolean
+    containerClass?: string
+    elevation?: number
+
+    // Titulo
+    title?: string
+
+    // Tabla
+    headers: Header[]
+    items: Record<string, any>[]
+
+    // Available
+    availableTrueText?: string
+    availableFalseText?: string
+    availableTrueColor?: string
+    availableFalseColor?: string
+  }>(), {
+    fluid: true,
+    containerClass: '',
+    elevation: 2,
+    title: '',
+    availableTrueText: 'Disponible',
+    availableFalseText: 'No disponible',
+    availableTrueColor: 'success',
+    availableFalseColor: 'error',
+  })
+</script>
--- frontend/src/pages/solicitudes.vue
+++ frontend/src/pages/solicitudes.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    <h1>Solicitudes</h1>
-  </div>
+  <!-- Tabla de Items -->
+  <ItemsTable
+    :headers="headers"
+    :items="items"
+    :search="search"
+    title="Solicitudes"
+  >
+    <!-- Implementar Acciones -->
+    <!-- <template #item.actions="{ item }"> -->
+    <!--   <v-btn color="amber-lighten-2" @click="function(item)"> -->
+    <!--     accion -->
+    <!--   </v-btn> -->
+    <!--   <v-divider class="mx-2" vertical /> -->
+    <!-- </template> -->
+  </ItemsTable>
 </template>
 
 <script setup lang="ts">
+  import ItemsTable from '@/components/ItemsTable.vue'
 
+  const headers = [
+    { title: 'ID', key: 'id', align: 'start' },
+    { title: 'Nombre', key: 'name', align: 'start' },
+    { title: 'Fecha del Pedido', key: 'available', align: 'center' },
+    { title: 'Fecha de Entrega', key: 'available', align: 'center' },
+    { title: 'Estado', key: 'available', align: 'center' },
+    { title: 'Acciones', key: 'actions', align: 'center', sortable: false },
+  ]
 </script>
 
 <style scoped>
-
 </style>
