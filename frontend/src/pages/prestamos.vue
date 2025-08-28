c6a3dd67ad935a5d9f472e6b5670dd65ab858cf2
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-08-27 22:44:21 -0400

    refactor(ItemsTable): extraer componente de *Items*
    Extraer componente de *Items* para views:
    - *Prestamo*
    - *Inventario*
    - *Solicitar*
    - *Solicitudes*
    - *Usuarios*

--- frontend/src/pages/prestamos.vue
+++ frontend/src/pages/prestamos.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    <h1>Prestamos</h1>
-  </div>
+  <!-- Tabla de Items -->
+  <ItemsTable
+    :headers="headers"
+    :items="items"
+    :search="search"
+    title="Prestamos"
+  />
 </template>
 
 <script setup lang="ts">
-
+  import ItemsTable from '@/components/ItemsTable.vue'
+  const headers = [
+    { title: 'ID', key: 'id', align: 'start' },
+    { title: 'Nombre', key: 'name', align: 'start' },
+    { title: 'Estado', key: 'available', align: 'center' },
+  ]
 </script>
 
 <style scoped>
-
 </style>
