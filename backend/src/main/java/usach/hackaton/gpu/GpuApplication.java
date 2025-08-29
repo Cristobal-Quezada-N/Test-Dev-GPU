c29f48c05f622b1b87decd1f49e6b18eb8bba53b
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-08-29 00:38:55 -0400

    chore: agregar `.env.development` para testeo

--- backend/src/main/java/usach/hackaton/gpu/GpuApplication.java
+++ backend/src/main/java/usach/hackaton/gpu/GpuApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GpuApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
+        Dotenv dotenv = Dotenv.configure()
+            .filename(".env.development") // Debug
+            .load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(GpuApplication.class, args);
 	}
