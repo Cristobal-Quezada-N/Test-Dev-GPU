20658583e5951f0dca2e172136738b54a1f4eb7e
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-10-02 20:13:35 -0300

    refactor(Security Config): extraer y modularizar manejadores de excepciones
    - `http.exceptionHandling()`

--- backend/src/main/java/usach/hackaton/gpu/config/SecurityConfig.java
+++ backend/src/main/java/usach/hackaton/gpu/config/SecurityConfig.java
@@ -1,8 +1,16 @@
 package usach.hackaton.gpu.config;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -10,21 +18,29 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
     private final JwtFilter jwtFilter;
     private final CorsConfigurationSource corsConfigurationSource;
+    private final ObjectMapper objectMapper;
 
-    public SecurityConfig(JwtFilter jwtFilter, CorsConfigurationSource corsConfigurationSource) {
+    public SecurityConfig(JwtFilter jwtFilter, CorsConfigurationSource corsConfigurationSource, ObjectMapper objectMapper) {
         this.jwtFilter = jwtFilter;
         this.corsConfigurationSource = corsConfigurationSource;
+        this.objectMapper = objectMapper;
     }
 
     @Bean
@@ -42,19 +58,40 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(ex -> ex
-                    .authenticationEntryPoint((req, res, e)-> {
-                        res.setStatus(401);
-                        res.setContentType("application/json");
-                        res.getWriter().write("{\"error\":\"unauthenticated\",\"reason\":\"" + e.getMessage() + "\"}");
-                    })
-                    .accessDeniedHandler((req, res, e) -> {
-                        res.setStatus(403);
-                        res.setContentType("application/json");
-                        res.getWriter().write("{\"error\":\"forbidden\",\"reason\":\"" + e.getMessage() + "\"}");
-                    })
+                .exceptionHandling(exeception -> exeception
+                    .authenticationEntryPoint((request, response, exception) -> handleAuthenticationException(request, response, exception))
+                    .accessDeniedHandler((request, response, exception) -> handleAccessDeniedException(request, response, exception))
                 );
         return http.build();
+    }
+
+    private void handleAuthenticationException(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AuthenticationException exception
+    ) throws IOException {
+        writeErrorResponse(request, response, HttpStatus.UNAUTHORIZED, "Unathenticated", exception.getMessage());
+    }
+
+    private void handleAccessDeniedException(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AccessDeniedException exception
+    ) throws IOException {
+        writeErrorResponse(request, response, HttpStatus.FORBIDDEN, "Forbidden", exception.getMessage());
+    }
+
+    private void writeErrorResponse(HttpServletRequest request, HttpServletResponse response, HttpStatus httpStatus, String error, String message) throws IOException {
+        response.setStatus(httpStatus.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("error", error);
+        errorResponse.put("message", message);
+        errorResponse.put("path", request.getRequestURI());
+        errorResponse.put("timestamp", LocalDateTime.now().toString());
+        
+        objectMapper.writeValue(response.getWriter(), errorResponse);
     }
 
     private void configurePublicEndpoints(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
