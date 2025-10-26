536d0e1f6e26a301ff0f18e5cfd671e5ebf217bb
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-10-26 20:45:35 -0300

    feat(test): implementar mocks de *mail sender* para uso de paquete test

--- backend/src/test/java/usach/hackaton/gpu/GpuApplicationTests.java
+++ backend/src/test/java/usach/hackaton/gpu/GpuApplicationTests.java
@@ -2,12 +2,19 @@ package usach.hackaton.gpu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import usach.hackaton.gpu.config.JwtUtil;
 
 @SpringBootTest
 class GpuApplicationTests {
+    @MockitoBean
+    private JwtUtil jwtUtil;
+
+    @MockitoBean
+    private JavaMailSender javaMailSender;
 
     @Test
     void contextLoads() {
     }
-
 }
