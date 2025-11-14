865fff6beb1e67d7959c9ea52a454f50f49db8b5
Cristóbal Quezada <cristobal.quezada.n@usach.cl>
2025-11-14 15:40:54 -0300

    refactor(AuthFactorService): reimplementar constructor con lombok

--- backend/src/main/java/usach/hackaton/gpu/service/AuthFactorService.java
+++ backend/src/main/java/usach/hackaton/gpu/service/AuthFactorService.java
@@ -1,16 +1,14 @@
 package usach.hackaton.gpu.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import usach.hackaton.gpu.entities.AuthFactor;
 import usach.hackaton.gpu.repositories.AuthFactorRepository;
 
 @Service
+@RequiredArgsConstructor
 public class AuthFactorService {
     private final AuthFactorRepository authFactorRepository;
-
-    public AuthFactorService(AuthFactorRepository authFactorRepository) {
-        this.authFactorRepository = authFactorRepository;
-    }
 
     public void save(AuthFactor authFactor) {
         authFactorRepository.save(authFactor);
